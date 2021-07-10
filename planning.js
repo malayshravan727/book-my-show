709da8e76cbfc56e0a01fd64ae355979930d689d
malayshravan727 <malayshravan727@gmail.com>
2021-07-10 19:26:46 +0530

    add HOC & layout

---
+++ planning.js
@@ -0,0 +1,23 @@
+//Book my show planning
+
+//we need a reusable carousal
+// ->Hero carousel
+// ->Common carousel
+// ->cast & crew small carousel
+
+//Pages
+// ->Home page
+// ->Movie page
+// ->Categories page
+// ->Auth. page
+// ->Razorpay payment gateway
+
+//Plan
+//Layouts
+//Routes
+//Pages
+//Components
+
+
+//HOC
+// ->Higher Order Components
---
+++ src/components/temp.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Temp() {
+  return <h1 className="text-2xl">Hey! Hi..</h1>;
+}
+
+export default Temp;
