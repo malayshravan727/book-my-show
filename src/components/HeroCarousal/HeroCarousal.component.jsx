8213e090a177b788d98709bf7f6b32c09d072696
malayshravan727 <malayshravan727@gmail.com>
2021-07-13 20:04:03 +0530

    add navbar and carousal

---
+++ src/components/HeroCarousal/HeroCarousal.component.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import HeroSlider from "react-slick";
+
+// Import css files
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+const HeroCarousal = () => {
+  const settings = {
+    arrows: true,
+    centerMode: true,
+    centerPadding: "200px",
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
+  const images = [
+    "https://images.unsplash.com/photo-1626160200858-0579929f1d6d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80",
+    "https://images.unsplash.com/photo-1626130521380-bb97c6695c66?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80",
+    "https://images.unsplash.com/photo-1593642634524-b40b5baae6bb?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=889&q=80",
+    "https://images.unsplash.com/photo-1626108728634-1e3a423a3541?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80",
+    "https://images.unsplash.com/photo-1626106063727-5369334359f1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80",
+    "https://images.unsplash.com/photo-1621052093608-a12172fcb80f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80",
+    "https://images.unsplash.com/photo-1625923782980-19cf8e864704?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=358&q=80",
+    "https://images.unsplash.com/photo-1626159727343-f8f700b7715b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80",
+];
+
+  return (
+    <>
+      <HeroSlider {...settings}>
+        {images.map((image) => (
+          <div className="w-20 h-80">
+            <img src={image} alt="testing" className="w-full h-full" />
+          </div>
+        ))}
+      </HeroSlider>
+    </>
+  );
+};
+
+export default HeroCarousal;
--- src/layouts/Default.layout.jsx
+++ src/layouts/Default.layout.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 
+// Components
+import Navbar from "../components/Navbar/navbar.component";
+import HeroCarousal from "../components/HeroCarousal/HeroCarousal.component";
+
 const DefaultLayout = (props) => {
-    return (
-        <>
-            <h1 className="text-xl">Default Layout</h1>
-            {props.children}
-        </>
-    );
+  return (
+    <>
+        <h1 className="text-xl">Default Layout</h1>
+        <Navbar />
+        <HeroCarousal />
+        {props.children}
+    </>
+  );
 };
 
 export default DefaultLayout;
