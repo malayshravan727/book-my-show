4e8e768476d8398c051e27e6bac738674602e518
malayshravan727 <malayshravan727@gmail.com>
2021-07-22 19:53:39 +0530

    add movie context & razorpay gateway

--- src/components/MovieHero/MovieInfo.component.jsx
+++ src/components/MovieHero/MovieInfo.component.jsx
@@ -1,9 +1,33 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 
+// components
+import PaymentModal from "../PaymentModal/Payment.component";
+
+// Context
+import { MovieContext } from "../../context/movie.context";
 
 const MovieInfo = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [price, setPrice] = useState(0);
+
+  const { movie } = useContext(MovieContext);
+
+  // optional chaining.
+  const genres = movie.genres?.map(({ name }) => name).join(", ");
+
+  const rentMovies = () => {
+    setIsOpen(true);
+    setPrice(149);
+  };
+
+  const buyMovies = () => {
+    setIsOpen(true);
+    setPrice(689);
+  };
+
   return (
     <>
+      <PaymentModal setIsOpen={setIsOpen} isOpen={isOpen} price={price} />
       <div className="flex flex-col gap-3 lg:gap-8">
         <div className="flex items-center gap-3 md:px-4">
           <div className="w-40 h-8">
@@ -18,20 +42,26 @@ const MovieInfo = () => {
           </span>
         </div>
         <h1 className="text-white lg:text-5xl font-bold hidden lg:block ">
-          Zack Snyder's Justice League
+          {movie.original_title}
         </h1>
         <div className="flex flex-col-reverse gap-3 lg:gap-5 lg:flex-col">
           <div className="text-white font-light flex flex-col gap-2 md:px-4">
-            <h4>English &bull; Languages: <span className="text-red-900 font-bold">Audio(1),Subtitles(1)</span></h4>
-            <h4>1h 53m &bull; Action, Advanture, Fantasy &bull; 16+ &bull; 18 Mar, 2021</h4>
+            <h4>4k &bull; {movie.original_language}</h4>
+            <h4>
+              {(movie.runtime / 60).toFixed(2)} h &bull; {genres} &bull; 13+
+            </h4>
           </div>
           <div className="flex items-center gap-3 md:px-4 md:w-screen lg:w-full">
-            <button className="bg-red-600 w-full py-3 text-white font-semibold rounded-lg">
-              Rent ₹149
-            </button>
-            <button className="bg-red-600 w-full py-3 text-white font-semibold rounded-lg">
-              Buy ₹689
-            </button>
+          <button 
+             onClick={rentMovies}
+             className="bg-red-600 w-full py-3 text-white font-semibold rounded-lg">
+             Rent ₹149
+          </button>
+          <button
+             onClick={buyMovies}
+             className="bg-red-600 w-full py-3 text-white font-semibold rounded-lg">
+             Buy ₹689
+          </button>
           </div>
         </div>
       </div>
